efa5464cdd3247e2f6ffd5646c2cf91630b8beb7
Juan Mendoza <juanmendoza0309@gmail.com>
2021-05-25 14:04:47 -0700

    getting up to date

--- src/main/java/com/revature/ATeamORM/repos/ObjectRepo.java
+++ src/main/java/com/revature/ATeamORM/repos/ObjectRepo.java
@@ -1,22 +1,27 @@
 package com.revature.ATeamORM.repos;
 
 import com.revature.ATeamORM.exceptions.DataSourceException;
+import com.revature.ATeamORM.util.Entity;
+import com.revature.ATeamORM.util.Id;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ObjectRepo {
-
+    
     public void save(Connection conn, Object object, String fromObject, String tableName, ArrayList<String> columns) {
-
+        
         object.getClass();
-
+        
         String[] objects = fromObject.split(",");
-
-        try{
+        
+        try {
             StringBuilder sql = new StringBuilder("insert into " + tableName + " (");
             for (int i = 0; i < columns.size(); i++) {
                 sql.append(columns.get(i));
@@ -32,20 +37,23 @@ public class ObjectRepo {
                 }
             }
             sql.append(")");
-
-            PreparedStatement pstmt = conn.prepareStatement(String.valueOf(sql), new String[] { "user_id" });
+            
+            PreparedStatement pstmt = conn.prepareStatement(String.valueOf(sql), new String[]{"user_id"});
             int rowsInserted = pstmt.executeUpdate();
-
+            
             if (rowsInserted != 0) {
                 ResultSet rs = pstmt.getGeneratedKeys();
                 while (rs.next()) {
- //                   object.setId(rs.getInt("user_id"));
+                    //                   object.setId(rs.getInt("user_id"));
                 }
             }
-
+            
         } catch (SQLException e) {
             throw new DataSourceException();
         }
-
+        
     }
+    
+    
+    
 }
