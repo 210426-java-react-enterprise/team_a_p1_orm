9ff63e5a328bc6dd84b7a9fb338fc38531f2a693
Vinson <55514029+vinsonchin@users.noreply.github.com>
2021-05-25 07:58:26 -0600

    added all CRUD methods; might want to change

--- src/main/java/com/revature/ATeamORM/repos/ObjectRepo.java
+++ src/main/java/com/revature/ATeamORM/repos/ObjectRepo.java
@@ -8,51 +8,155 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Map;
 
 public class ObjectRepo {
 
-    public void save(Connection conn, Object object, String fromObject, String tableName, ArrayList<String> columns) throws NoSuchMethodException {
-
-        Class theClass = object.getClass();
-        Method method = theClass.getMethod("setId",Integer.class);
-
-        String[] objects = fromObject.split(",");
+    public void create(Connection conn, Object object, String tableName, Map<String, String> columns) {
 
         try{
+
+            Class theClass = object.getClass();
+            Method method = theClass.getMethod("setId",Integer.class);
+
             StringBuilder sql = new StringBuilder("insert into " + tableName + " (");
-            for (int i = 0; i < columns.size(); i++) {
-                sql.append(columns.get(i));
-                if (i < columns.size() - 1) {
+            int i = 1;
+            for (String column: columns.keySet()) {
+                sql.append(column);
+                if (i < columns.size()) {
                     sql.append(", ");
                 }
+                i++;
             }
             sql.append(") values (");
-            for (int i = 0; i < objects.length; i++) {
-                sql.append(objects[i]);
-                if (i < columns.size() - 1) {
+            i = 1;
+            for (String value: columns.values()) {
+                sql.append(value);
+                i++;
+                if (i < columns.size()) {
                     sql.append(", ");
                 }
             }
             sql.append(")");
 
-            PreparedStatement pstmt = conn.prepareStatement(String.valueOf(sql), new String[] { "user_id" });
+            PreparedStatement pstmt = conn.prepareStatement(String.valueOf(sql), new String[] { "id" });
             int rowsInserted = pstmt.executeUpdate();
 
             if (rowsInserted != 0) {
                 ResultSet rs = pstmt.getGeneratedKeys();
                 while (rs.next()) {
-                    method.invoke(object,rs.getInt("user_id"));
+                    method.invoke(object,rs.getInt("id"));
                 }
             }
 
         } catch (SQLException e) {
             throw new DataSourceException();
         } catch (InvocationTargetException e) {
-            e.printStackTrace();
+            System.out.println("Cannot invoke method");
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            System.out.println("Cannot access that object");
+        } catch (NoSuchMethodException e) {
+            System.out.println("No such method exists");
         }
 
     }
+
+    public ResultSet read(Connection conn, String table, Map<String, String> columns) {
+
+        ResultSet rs = null;
+
+        try {
+
+            StringBuilder sql = new StringBuilder("select * from " + table + " where ");
+            int i = 1;
+            for (String column: columns.keySet()) {
+                sql.append(column + " = " + columns.get(column));
+                if (i < columns.size()) {
+                    sql.append(" and ");
+                }
+                i++;
+            }
+
+            if(conn == null) {
+                throw new NullPointerException(System.getProperty("host_url") +
+                        " is what has been given as the host url from environment variables \n and the username is: "+
+                        System.getProperty("db_username") + " with a password of: "+System.getProperty("db_password"));
+            }
+
+            PreparedStatement pstmt = conn.prepareStatement(String.valueOf(sql));
+
+            rs = pstmt.executeQuery();
+
+        } catch (SQLException e) {
+            throw new DataSourceException();
+        }
+
+        return rs;
+
+    }
+
+    public void update(Connection conn, Object object, String table, Map<String, String> columns) {
+
+        try {
+
+            Class theClass = object.getClass();
+            Method method = theClass.getMethod("getId",Integer.class);
+            Method otherMethod = theClass.getMethod("setId",Integer.class);
+
+            StringBuilder sql = new StringBuilder("update "  + table + " set ");
+            int i = 1;
+            for (String column: columns.keySet()) {
+                sql.append(column + " = " + columns.get(column));
+                if (i < columns.size()) {
+                    sql.append(", ");
+                }
+            }
+            sql.append(" where id = " + method.invoke(object));
+
+            PreparedStatement pstmt = conn.prepareStatement(String.valueOf(sql));
+
+            int rowsUpdated = pstmt.executeUpdate();
+
+            if (rowsUpdated != 0) {
+                ResultSet rs = pstmt.getGeneratedKeys();
+                while (rs.next()) {
+                    otherMethod.invoke(object, rs.getInt("id"));
+                }
+            }
+
+        } catch (NoSuchMethodException e) {
+            System.out.println("No such method exists");
+        } catch (InvocationTargetException e) {
+            System.out.println("Cannot invoke that method");
+        } catch (IllegalAccessException e) {
+            System.out.println("Cannot access that object");
+        } catch (SQLException e) {
+            throw new DataSourceException();
+        }
+    }
+
+    public void delete(Connection conn, Object object, String table) {
+
+        try {
+
+            Class theClass = object.getClass();
+            Method method = theClass.getMethod("getId",Integer.class);
+
+            String sql = "delete from " + table + " where id = " + method.invoke(object);
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            int rowsUpdated = pstmt.executeUpdate();
+
+        } catch (NoSuchMethodException e) {
+            System.out.println("No such method exists");
+        } catch (InvocationTargetException e) {
+            System.out.println("Cannot invoke that method");
+        } catch (IllegalAccessException e) {
+            System.out.println("Cannot access that object");
+        } catch (SQLException e) {
+            throw new DataSourceException();
+        }
+    }
+
 }
